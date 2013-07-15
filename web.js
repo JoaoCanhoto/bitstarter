563618846e8f7cc715a9f33ce774761000d844cb
Joao Espadanal <joaopedroespadanal@gmail.com>
2013-07-15 15:42:06 +0000

    h3.1.0

--- web.js
+++ web.js
@@ -5,23 +5,27 @@ var app = express.createServer(express.logger());
 
 fs = require('fs');
 buf = new Buffer(256);
-fs.readFileSync('index.html', 'utf8', function (err,data) {
+fs.readFile('./index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
 //  console.log(data);
-buff=data;
+buf=data;
+//console.log(buf.toString('utf8')  );
 });
 
 
 
 app.get('/', function(request, response) {
-//  response.send( buf.toString('utf8', 0, buff)  );
-  response.send("Hello World2.2!");
+  response.send( buf.toString('utf8') ) ; //, 0, buff)  );
+//  response.send("Hello World2.2!");
 });
 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+B
+B
 });
+
